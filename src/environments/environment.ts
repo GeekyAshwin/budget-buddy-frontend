42365a3c5d89f0ab110f2cebd1d519762c4f1f16
Ashwiner <techmindashwin78@gmail.com>
2024-08-18 14:04:49 +0530

    modified env key for server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
  const Environment = {
   API_URL: "https://budgetbuddy2024-2024.onrender.com/api/",
-   SERVER_PUBLIC_KEY: "BJP5Q0VvPXmgZMjSGjuQEAXSDwHOeklNdQovxPxa5TEyLQcGBAxxy2yp1rudVrMLNM1dUDUIZHpO394smtmkB04"
+   SERVER_PUBLIC_KEY: "BA3ZbWf-mLnvcT6fGPLgjUmIQKPvgN0uhB6NzWoEyZHAWe3B3MKXsPkRG7nfGGWX4gG3V_jKN5cElCLh_iSIhVA"
 }
 export default Environment;
